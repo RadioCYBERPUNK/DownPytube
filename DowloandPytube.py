a51b2e5436eaeaccb72db85ee27c4b84b7ad5b47
Radio CYBERPUNK <144034769+RadioCYBERPUNK@users.noreply.github.com>
2023-09-20 21:56:53 -0300

    ScriptDownlalalalaDedaoGabis

---
+++ DowloandPytube.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+
+# URL do vídeo que você deseja baixar
+url = 'https://www.youtube.com/watch?v=EBCsYTcZB6c'
+
+# Crie um objeto YouTube
+yt = YouTube(url)
+
+stream = yt.streams.get_by_itag('22')  
+
+# Baixe o vídeo
+stream.download(output_path='C:\\Users\\saturno\\Documents', filename='teste2')
+
+print('Download concluído!')
